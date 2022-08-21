f0a99d27cbfbcd9ca538caffcac264685ca070a3
pathtofile <git@tofile.dev>
2022-08-21 15:57:06 +1000

    working formula

--- Formula/toucli.rb
+++ Formula/toucli.rb
@@ -1,14 +1,12 @@
 cask "toucli" do
     version "1.0"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-
+    sha256 "9f544ce9d9f3100054745377759303213744b5ed9e7940d956efed3724696868"
+  
     url "https://github.com/pathtofile/toucli/releases/download/v#{version}/toucli.zip"
-    name "Toucli"
+    name "toucli"
     desc "Use touchID and the Secure Enclave to encrypt data from the commandline"
     homepage "https://github.com/pathtofile/toucli"
   
     depends_on macos: ">= :big_sur"
-  
     app "toucli.app"
-  
   end
